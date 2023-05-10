b406ce3da7887dfbaaade62cd51dcff2fc8e5252
Aimee Barciauskas <aimee@developmentseed.org>
2023-05-09 19:17:59 -0700

    Add fixture for zarr

--- test_factory.py
+++ test_factory.py
@@ -9,12 +9,7 @@ xarray_factory = XarrayTilerFactory()
 app.include_router(xarray_factory.router)
 client = TestClient(app)
 DATA_DIR = 'fixtures'
-
-def contains_sublist(main_list, sublist):
-    if len(sublist) > len(main_list):
-        return False
-    # Check if the sublist exists in the main_list
-    return any(main_list[i:i + len(sublist)] == sublist for i in range(len(main_list) - len(sublist) + 1))
+test_zarr_store = f"{DATA_DIR}/test_zarr_store.zarr"
 
 def test_get_variables_reference():
     # With reference file
@@ -27,15 +22,14 @@ def test_get_variables_reference():
     assert response.json() == ['anom', 'err', 'ice', 'sst']
 
 def test_get_variables():
-    power_zarr = f"{DATA_DIR}/power_901_monthly_meteorology_utc.zarr"
     response = client.get("/variables", params={
-        "url": power_zarr,
+        "url": test_zarr_store,
         "decode_times": False
     }) 
     assert response.status_code == 200
-    sublist = ['CDD0', 'CDD10', 'CDD18_3', 'DISPH']
+    sublist = ['CDD0', 'DISPH', 'FROST_DAYS', 'GWETPROF']
     main_list = response.json()
-    assert contains_sublist(main_list, sublist)
+    assert main_list == sublist
 
 # pytest test_factory.py::test_get_info
 def test_get_info_reference():
@@ -51,14 +45,13 @@ def test_get_info_reference():
         assert response.json() == json.load(f)
 
 def test_get_info():
-    power_zarr = f"{DATA_DIR}/power_901_monthly_meteorology_utc.zarr"
     response = client.get("/info", params={
-        "url": power_zarr,
-        "variable": "WS50M",
+        "url": test_zarr_store,
+        "variable": "CDD0",
         "decode_times": False
     }) 
     assert response.status_code == 200
-    with open(f"{DATA_DIR}/responses/power_901_monthly_meteorology_utc_WS50M_info.json") as f:
+    with open(f"{DATA_DIR}/responses/test_CDD0_info.json") as f:
         assert response.json() == json.load(f)
 
 def test_get_tilejson_reference():
@@ -73,17 +66,17 @@ def test_get_tilejson_reference():
         assert response.json() == json.load(f)
 
 def test_get_tilejson():
-    power_zarr = f"{DATA_DIR}/power_901_monthly_meteorology_utc.zarr"
     response = client.get("/tilejson.json", params={
-        "url": power_zarr,
-        "variable": "WS50M",
+        "url": test_zarr_store,
+        "variable": "CDD0",
         "decode_times": False
     }) 
     assert response.status_code == 200
-    with open(f"{DATA_DIR}/responses/power_901_monthly_meteorology_utc_WS50M_tilejson.json") as f:
+    with open(f"{DATA_DIR}/responses/test_CDD0_tilejson.json") as f:
         assert response.json() == json.load(f)
 
-# TODO(aimee): These depend on connectivity, possible to do without?
+# TODO(aimee): This depends on connectivity + access to NOAA OISST files
+# We should store mock data for the kercunk reference in fixtures/.
 def test_get_tile_reference():
     noaa_oisst_reference = f"{DATA_DIR}/noaa_oisst_reference.json"
     response = client.get("/tiles/0/0/0.png", params={
@@ -95,10 +88,9 @@ def test_get_tile_reference():
     assert response.headers['Content-Type'] == 'image/png'
 
 def test_get_tile():
-    power_zarr = "s3://power-analysis-ready-datastore/power_901_monthly_meteorology_utc.zarr"
     response = client.get("/tiles/0/0/0.png", params={
-        "url": power_zarr,
-        "variable": "WS50M",
+        "url": test_zarr_store,
+        "variable": "CDD0",
         "decode_times": False
     }) 
     assert response.status_code == 200
