822889b620ef8ab46935fae54fe28e3204b3f35e
Aimee Barciauskas <aimee@developmentseed.org>
2023-09-25 16:35:07 -0700

    Try catch recursion error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,23 @@
 """titiler.xarray tests configuration."""
-
+from contextlib import contextmanager
 import pytest
 from fastapi.testclient import TestClient
+from typing import Iterator
+from pydantic import ValidationError
 
+def is_recursion_validation_error(exc: ValidationError) -> bool:
+    errors = exc.errors()
+    return len(errors) == 1 and errors[0]['type'] == 'recursion_loop'
 
 @pytest.fixture
 def app(monkeypatch):
     """App fixture."""
     monkeypatch.setenv("TITILER_XARRAY_DEBUG", "TRUE")
-
-    from titiler.xarray.main import app
+    try:
+        from titiler.xarray.main import app
+    except ValidationError as exc:
+        if not (is_recursion_validation_error(exc)):
+            raise exc        
 
     with TestClient(app) as client:
         yield client
