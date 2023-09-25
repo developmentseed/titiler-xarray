f28352844e135ec95946779a4da0ff5d85023bcd
Aimee Barciauskas <aimee@developmentseed.org>
2023-09-25 15:06:58 -0700

    Add histogram response and test helpers file

---
+++ tests/helpers.py
@@ -0,0 +1,15 @@
+import httpx
+
+def find_string_in_stream(response: httpx.Response, target: str) -> bool:
+    # Ensure the response is valid
+    response.raise_for_status()
+    buffer = ""
+    # Read the response in chunks
+    for chunk in response.iter_bytes():
+        buffer += chunk.decode()
+        # Check if the target string is in our buffer
+        if target in buffer:
+            return True
+        # Optional: To avoid the buffer getting too large, you can clear it or manage its size
+        buffer = buffer[-len(target):]  # Keep only the tail end of the buffer with length equal to the target string
+    return False
