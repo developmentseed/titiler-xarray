01636dfe3faea3e7cfaa4187a9c6497c0e6b3935
Aimee Barciauskas <aimee@developmentseed.org>
2023-09-26 15:05:58 -0700

    Run unused function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,14 @@
 """titiler.xarray tests configuration."""
-from contextlib import contextmanager
+
 import pytest
 from fastapi.testclient import TestClient
-from typing import Iterator
 from pydantic import ValidationError
-
-def is_recursion_validation_error(exc: ValidationError) -> bool:
-    errors = exc.errors()
-    return len(errors) == 1 and errors[0]['type'] == 'recursion_loop'
 
 @pytest.fixture
 def app(monkeypatch):
     """App fixture."""
     monkeypatch.setenv("TITILER_XARRAY_DEBUG", "TRUE")
-    
+
     from titiler.xarray.main import app
 
     with TestClient(app) as client:
