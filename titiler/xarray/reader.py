f81c006cb11bfcfbefd6b927d9efdbdcf7c7177d
Aimee Barciauskas <aimee@developmentseed.org>
2023-10-26 09:45:52 -0700

    Fix transpose code

--- titiler/xarray/reader.py
+++ titiler/xarray/reader.py
@@ -163,13 +163,10 @@ def get_variable(
     if ds.dims.get("longitude"):
         longitude_var_name = "longitude"
     da = da.rename({latitude_var_name: "y", longitude_var_name: "x"})
-    required_dims = ["time", "y", "x"]
-    if not all(dim in da.dims for dim in required_dims):
-        raise Exception(
-            f"Dataset must have the following dimensions: {required_dims}"
-        )
-    if da.dims != required_dims:
-        da = da.transpose(*required_dims)
+    dims_order = list(da.dims)
+    # Check if 'y' comes before 'x'
+    if dims_order.index('y') > dims_order.index('x'):
+        da = da.transpose('y', 'x')
 
     # TODO: add test
     if drop_dim:
