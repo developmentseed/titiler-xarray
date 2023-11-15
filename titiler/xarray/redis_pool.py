663c2225e9d6a39f9193e6ae49b1ffa361c9bbb4
Aimee Barciauskas <aimee@developmentseed.org>
2023-11-14 16:22:08 -0800

    Add redis pool

---
+++ titiler/xarray/redis_pool.py
@@ -0,0 +1,24 @@
+from titiler.xarray.settings import ApiSettings
+import redis
+
+api_settings = ApiSettings()
+
+class RedisCache:
+    _instance = None
+
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = redis.ConnectionPool(
+                host=api_settings.cache_host, 
+                port=6379, 
+                # Do we need to specify the db index here?
+                # db=0
+            )
+        return cls._instance
+
+def get_redis():
+  pool = RedisCache.get_instance()
+  # Here, we re-use our connection pool
+  # not creating a new one
+  return redis.Redis(connection_pool=pool)
