b2e83ec01494eacdca570101e38bb022a9d5c535
EC2 Default User <ec2-user@ip-172-31-43-161.us-west-2.compute.internal>
2024-04-23 14:46:22 +0000

    propagate cache option to docker builds

--- infrastructure/aws/cdk/app.py
+++ infrastructure/aws/cdk/app.py
@@ -132,14 +132,17 @@ class LambdaStack(Stack):
                 {"TITILER_XARRAY_CACHE_HOST": redis_cluster.attr_redis_endpoint_address}
             )
 
+        dockerfile_name = "Dockerfile.redis" if settings.enable_cache else "Dockerfile"
+
         lambda_function = aws_lambda.Function(
             self,
             f"{id}-lambda",
             runtime=runtime,
             code=aws_lambda.Code.from_docker_build(
                 path=os.path.abspath(context_dir),
-                file="infrastructure/aws/lambda/Dockerfile",
+                file=f"infrastructure/aws/lambda/{dockerfile_name}",
                 platform="linux/amd64",
+                build_args={"ENABLE_CACHE": settings.enable_cache},
             ),
             handler="handler.handler",
             memory_size=memory,
